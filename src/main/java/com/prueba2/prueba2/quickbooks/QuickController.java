199c2ad60809d935fa457dc031492e188e75a63c
Diego Diaz De Leon <diegodiazdeleoncai@gmail.com>
2024-03-26 05:44:26 -0600

    quickbook firts class

--- src/main/java/com/prueba2/prueba2/quickbooks/QuickController.java
+++ src/main/java/com/prueba2/prueba2/quickbooks/QuickController.java
@@ -1,10 +1,56 @@
 package com.prueba2.prueba2.quickbooks;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Named
 @RequestScoped
 public class QuickController {
+    private OAuth2Config oauth2Config;
 
+    public void connectToQuickBooks() {
+        try {
+            Properties prop = new Properties();
+            InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties");
+            prop.load(input);
+            String clientId = prop.getProperty("OAuth2AppClientId");
+            String clientSecret = prop.getProperty("OAuth2AppClientSecret");
+            String redirectUri = prop.getProperty("OAuth2AppRedirectUri");
+
+            //initialize the config
+            oauth2Config = new OAuth2Config.OAuth2ConfigBuilder(clientId, clientSecret)
+                    .callDiscoveryAPI(Environment.SANDBOX)
+                    .buildConfig();
+
+            //generate csrf token
+            String csrf = oauth2Config.generateCSRFToken();
+
+            //prepare scopes
+            List<Scope> scopes = new ArrayList<Scope>();
+            scopes.add(Scope.Accounting);
+
+
+
+            //prepare authorization url to intiate the oauth handshake
+            String authorizationUrl =oauth2Config.prepareUrl(scopes, redirectUri, csrf);
+
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            externalContext.redirect(authorizationUrl);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Failed to connect to QuickBooks: " + ex.getMessage());
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        } catch (InvalidRequestException e) {
+            throw new RuntimeException(e);
+        }
 }
