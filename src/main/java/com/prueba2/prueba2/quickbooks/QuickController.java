85cd6b2ce513f4f7e0d0dddb8583625cd3e14bd7
Diego Diaz <dev5@sourcedevil.com>
2024-03-26 11:24:28 +0000

    quickbooks1

---
+++ src/main/java/com/prueba2/prueba2/quickbooks/QuickController.java
@@ -0,0 +1,10 @@
+package com.prueba2.prueba2.quickbooks;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class QuickController {
+
+}
